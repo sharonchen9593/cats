032b035c8a864e16d922a3e736ebe573e93bc605
sharonchen9593 <sharonchen9593@gmail.com>
2018-12-04 20:16:56 -0800

    Removed unused dependency

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         query: {
-          presets: ["react", "es2015", "react-hmre"],
+          presets: ["react", "env", "react-hmre"],
           plugins: ["transform-class-properties"]
         }
       },
